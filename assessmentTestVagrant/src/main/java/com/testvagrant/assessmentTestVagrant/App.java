97b4069a29a18c9747ab8f95fe16046af74a0461
lokaremonica <monilokare@gmail.com>
2021-04-26 23:04:32 +0530

    Initial Setup

---
+++ assessmentTestVagrant/src/main/java/com/testvagrant/assessmentTestVagrant/App.java
@@ -0,0 +1,13 @@
+package com.testvagrant.assessmentTestVagrant;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
